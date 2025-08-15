094272499c5e638b766a3843f4c16de457f27548
n4n5 <its.just.n4n5@gmail.com>
2025-08-15 11:36:31 -0600

    add files

---
+++ deploy.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+set -x
+
+if [ ! -d "typst" ]; then
+    echo "Typst directory does not exist. Cloning repository..."
+    git clone https://github.com/typst/typst.git
+    cd typst || exit 1
+    cargo run --package typst-docs -- --assets-dir assets/docs --out-file assets/docs.json --base /en/
+    cd ..
+fi
+
+if [ ! -d "typst-jp.github.io" ]; then
+    echo "Website directory does not exist. Cloning repository..."
+    git clone https://github.com/typst-jp/typst-jp.github.io.git
+    cd typst-jp.github.io || exit 1
+    cargo run --package typst-docs -- --assets-dir assets/docs --out-file assets/docs.json --base /en/
+    cd ..
+fi
+
+rm -rf dist/
+mkdir -p dist/
+
+if [ -d "website_builder" ]; then
+    rm -rf website_builder
+fi
+
+mkdir -p website_builder
+cp -r typst-jp.github.io/website website_builder/
+cd website_builder/website || exit 1
+npm install
+rm -rf public/index.html
+# delete the js redirection to /docs
+sed -i '29,31d' src/index.tsx
+sed -i 's|https://typst-jp.github.io/|https://typst-github-io.n4n5.dev/|g' vite.config.ts
+cd ../..
+
+cp -r typst/assets/ website_builder/assets/
+cd website_builder/website || exit 1
+npm run build
+mv dist/ ../../dist/en/
+cd ../..
+# sleep 20
+rm -rf website_builder/assets/
+cp -r typst-jp.github.io/assets/ website_builder/assets/
+cd website_builder/website || exit 1
+npm run build
+mv dist/ ../../dist/jp/
+cd ../..
+cp index.html dist/
+cp -r dist/jp/assets/ dist/assets
+mv dist/en/en/* dist/en/
+mv dist/en/assets/docs/* dist/en/assets/
+mv dist/jp/jp/* dist/jp/
+mv dist/jp/assets/docs/* dist/jp/assets/
